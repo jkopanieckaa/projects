34c572ed49746c07b02bad8d588ff310f281d72a
Judyta Kopaniecka <kopanieckajudyta@gmail.com>
2025-02-28 21:05:40 +0100

    finished from tutorial

--- memory_game/script.js
+++ memory_game/script.js
@@ -1,16 +1,76 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
+let matchedPairs = 0;
 
 function flipCard() {
-    this.classList.add('flip');
-    if(!hasFlippedCard){
-      hasFlippedCard = true;
-      firstCard = this;
+    if (lockBoard) return;
 
-      console.log({hasFlippedCard, firstCard});
-  }
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    }
+        //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch()
+
+    if(matchedPairs === 6) {
+        win();
+    }
 }
 
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    matchedPairs++;
+    console.log(matchedPairs);
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        lockBoard = false;
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach(card=> {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+//u won!!
+function win() {
+    setTimeout(() => {
+        alert("You won!");
+        location.reload();
+    }, 1000);
+
+}
 cards.forEach(card => card.addEventListener('click', flipCard));
